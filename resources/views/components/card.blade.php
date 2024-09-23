79406235fd5d1377360f3ad068a1ef12da90c49e
duskcus <60875989+duskcus@users.noreply.github.com>
2024-09-23 15:47:42 +0200

    Update card.blade.php

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,11 @@
+<div class="card bg-base-100 shadow-xl">
+    <figure>
+      <img
+        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+        alt="Shoes" />
+    </figure>
+    <div class="card-body">
+        {{ $slot }}
+      </div>
+    </div>
+  </div>
