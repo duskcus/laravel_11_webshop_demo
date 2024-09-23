5be805c1e113d7cd99c6ed942ba9f515e6e8225a
duskcus <60875989+duskcus@users.noreply.github.com>
2024-09-23 11:43:48 +0200

    Add files via upload

---
+++ database/migrations/2024_09_23_094153_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->integer('price');
+            $table->string('image')->nullable();
+            $table->integer('stock');
+            $table->string('category');
+            $table->timestamps(); // Standard to Laravel adds a created_at and updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
