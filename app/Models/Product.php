a26b2d71ec51639f13f72224316b6800d2e92465
duskcus <60875989+duskcus@users.noreply.github.com>
2024-09-23 12:01:33 +0200

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,35 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'products';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'image',
+        'stock',
+        'category',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'price' => 'integer',
+        'stock' => 'integer',
+    ];
 }
