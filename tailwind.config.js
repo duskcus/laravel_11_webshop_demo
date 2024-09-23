7b4f600279152393f05eafee857738d94496b1d6
duskcus <60875989+duskcus@users.noreply.github.com>
2024-09-23 11:33:09 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
+    // Add the daisyui configuration here
+    daisyui: {
+        themes: ["light", "dark"],
+    },
+  }
