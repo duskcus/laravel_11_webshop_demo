911dd5580f6617cbaeca373480ab43376597e0b2
duskcus <60875989+duskcus@users.noreply.github.com>
2024-09-23 11:40:03 +0200

    Update footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,6 @@
-@extends('layouts.default')
-
-{{-- @SECTION FOR THE START CONTENT --}}
-@section('content')
-    <section>
-        hello world.
-    </section>
-@endsection
+<!-- Footer -->
+<footer>
+    <div class="bg-primary text-center py-3 text-white">
+        <p>&copy; 2024 Your Website. All rights reserved.</p>
+    </div>
+</footer>
