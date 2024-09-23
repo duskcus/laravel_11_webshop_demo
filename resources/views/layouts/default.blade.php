35a716aecabcff19cb9631d08a2a3a6b328b3b01
duskcus <60875989+duskcus@users.noreply.github.com>
2024-09-23 11:37:41 +0200

    Update default.blade.php

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,1 +1,22 @@
+<!DOCTYPE html>
+<html data-theme="dark" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<meta charset="UTF-8">
+<title>{{ config('app.name', 'Laravel') }}</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<head>
+    @vite('resources/css/app.css')
+</head>
 
+<body class="mx-auto">
+    {{-- INCLUDES FOR COMPONENTS --}}
+    @include('components.header')
+
+    <main>
+        {{-- YIELD FOR CONTENT --}}
+        @yield('content')
+    </main>
+
+    @include('components.footer')
+
+</body>
+</html>
