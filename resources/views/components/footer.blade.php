3d3ff15bb3d14f1bb68eef8d12d6c9be24050fa1
duskcus <60875989+duskcus@users.noreply.github.com>
2024-09-25 13:10:24 +0200

    Update footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
  <!-- Footer -->
 <footer>
-    <div class="bg-base-100-content text-center py-3 text-base-content">
+    <div class="bg-base-100-content text-center py-4 text-base-content">
         <p>&copy; 2024 Your Website. All rights reserved.</p>
     </div>
 </footer>
