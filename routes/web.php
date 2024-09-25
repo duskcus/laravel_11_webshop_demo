496d4bec6070e014a18f9d2509e6b8b111f3bab4
duskcus <60875989+duskcus@users.noreply.github.com>
2024-09-25 11:37:09 +0200

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AuthController;
 
+// STANDARD
 Route::get('/', function () {
     return view('index');
 })->name('home');
@@ -20,9 +21,26 @@ Route::get('/dashboard', function () {
     return view('authentication.dashboard');
 })->name('dashboard');
 
+Route::get('/profile', function () {
+    return view('account.profile');
+})->name('profile');
+
+Route::get('/settings', function () {
+    return view('account.settings');
+})->name('settings');
+
 // RESOURCES
 Route::resource('products', ProductController::class);
 
-// AUTH
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::get('/register', [AuthController::class, 'register'])->name('register');
+// LOGIN
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+// REGISTER
+Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+
+// LOGOUT
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/api', [ProductController::class, 'api']);
