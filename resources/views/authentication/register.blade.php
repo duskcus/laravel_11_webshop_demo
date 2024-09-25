7e0a481f70315fb66da8af58d7afc4ebe1ef4def
duskcus <60875989+duskcus@users.noreply.github.com>
2024-09-25 13:23:29 +0200

    Delete resources/views/authentication directory

--- resources/views/authentication/register.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.no-nav-footer')
-
-@section('content')
-<section class="w-[30%] mx-auto py-8 px-12 bg-base-100 rounded-md shadow-md">
-
-    @livewire('stepper')
-
-</section>
-@endsection
