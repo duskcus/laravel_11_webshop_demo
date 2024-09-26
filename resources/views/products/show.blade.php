d598feb74eb694a6f74c44536a0cf9cb39aab5e8
duskcus <60875989+duskcus@users.noreply.github.com>
2024-09-26 14:23:22 +0200

    Update show.blade.php

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -3,12 +3,15 @@
 {{-- @SECTION FOR THE START CONTENT --}}
 @section('content')
     <section>
-        <img class="w-[300px]" src="{{ $product->image ?? 'https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp' }}" alt="Product Image">
-        <p>{{ $product->name }}</p>
-        <p>{{ $product->description }}</p>
-        <p>{{ $product->price }}</p>
-        <p>{{ $product->stock }}</p>
-        <p>{{ $product->category }}</p>
-
+        <div class="grid grid-cols-2 gap-6 p-6">
+        <img class="h-[300px]" src="{{ $product->image ?? 'https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp' }}" alt="Product Image">
+        <div>
+            <p class="text-2xl mb-6 capitalize">{{ $product->name }}</p>
+            <p>{{ $product->description }}</p>
+            <p>price: {{ $product->price }}</p>
+            <p>stock: {{ $product->stock }}</p>
+            <p>category: <a class="link" href="">{{ $product->category }}</a></p>
+        </div>
+        </div>
     </section>
 @endsection
