208486a784ed13293940ad057ab19a1a08d3e98f
duskcus <60875989+duskcus@users.noreply.github.com>
2024-09-26 14:24:49 +0200

    Update stepper.blade.php

--- resources/views/livewire/stepper.blade.php
+++ resources/views/livewire/stepper.blade.php
@@ -3,52 +3,61 @@
     <div class="text-center flex-shrink-0 my-6 mx-auto hover:opacity-75">
         <!-- Img -->
         {{-- <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" class="h-[50px] cursor-pointer"> --}}
-        <a href="{{ route('home') }}"><span class="font-semibold text-xl">LOGONAME</span></a>
+        {{-- <a href="{{ route('home') }}"><span class="font-semibold text-xl">LOGONAME</span></a> --}}
+        <span class="font-semibold text-xl">Register</span>
     </div>
     @if ($step === 1)
         {{-- Basic details --}}
         <div class="card card-normal w-full mx-auto">
             <div class="form-control mt-4">
                 <label for="email">E-mail</label>
-                <input class="input input-bordered w-full max-w-xs" type="email" wire:model.defer="formData.email" id="email">
+                <input class="input input-bordered w-full" type="email" wire:model.defer="formData.email" id="email">
                 @error('formData.email') <span class="text-error">E-mail field must be a valid email address.</span> @enderror
             </div>
             <div class="form-control mt-4">
                 <label for="password">Password</label>
-                <input class="input input-bordered w-full max-w-xs" type="password" wire:model.defer="formData.password" id="password">
+                <input class="input input-bordered w-full" type="password" wire:model.defer="formData.password" id="password">
                 @error('formData.password') <span class="text-error">Password must be at least 8 characters.</span> @enderror
             </div>
         </div>
     @elseif ($step === 2)
-        {{-- Profile and character --}}
-        <div class="form-control mt-4">
-            <label for="name">Name</label>
-            <input class="input input-bordered w-full max-w-xs" type="text" wire:model.defer="formData.name" id="name">
-            @error('formData.name') <span class="text-error">Username taken/empty</span> @enderror
+        {{-- Second auth --}}
+        <div class="form-control mt-4 text-center">
+            <div class="inline-flex mx-auto mb-4">
+                <a class="mx-3" href="https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2&pcampaignid=web_share"><i class="fa fa-android fa-5x"></i></a>
+                <a class="mx-3" href="https://apps.apple.com/nl/app/google-authenticator/id388497605"><i class="fa fa-apple fa-5x"></i></a>
+            </div>
+            <p>To finish your registration you will need to set-up second authentication. You can click these icon's to download google authenticator</p>
         </div>
     @elseif ($step === 3)
         {{-- Second auth --}}
         <div class="form-control mt-4">
-            <label for="name">Auth2</label>
-            <input class="input input-bordered w-full max-w-xs" type="text" wire:model.defer="formData.name" id="name">
-            @error('formData.name') <span class="text-error">Incorrect/expired code</span> @enderror
+            <label for="name">Fill in your code:</label>
+        </div>
+    @elseif ($step === 4)
+        {{-- Profile and character --}}
+        <div class="form-control mt-4">
+            <label for="name">Name</label>
+            <input class="input input-bordered w-full" type="text" wire:model.defer="formData.name" id="name">
+            @error('formData.name') <span class="text-error">Username taken/empty</span> @enderror
         </div>
     @endif
 
 
     <div class="step-navigation mt-4">
-        @if ($step > 1)
-            <button class="btn btn-primary" wire:click="previousStep">Previous</button>
-        @endif
-        @if ($step < 3)
-            <button class="btn btn-primary" wire:click="nextStep">Next</button>
+        @if ($step < 4)
+            <button class="btn btn-primary w-full mb-3" wire:click="nextStep">Next</button>
         @else
-            <button class="btn btn-primary" wire:click="submit">Submit</button>
+            <button class="btn btn-primary w-full mb-3" wire:click="submit">Submit</button>
+        @endif
+        @if ($step > 1)
+            <button class="btn btn-error w-full" wire:click="previousStep">Previous</button>
         @endif
 
-
+        @if ($step === 1)
         <div class="text-center mt-4">
-            <a href="{{ route('login') }}" class="link link-primary">Already have an account?</a>
+            <a href="{{ route('login') }}" class="link">Already have an account?</a>
         </div>
+        @endif
     </div>
 </div>
