62a38079cb659a4cd78996afa0f7ed7cc7cf4117
duskcus <60875989+duskcus@users.noreply.github.com>
2024-09-26 14:31:22 +0200

    Update profile.blade.php

--- resources/views/account/profile.blade.php
+++ resources/views/account/profile.blade.php
@@ -4,5 +4,8 @@
 @section('content')
     <section class="mx-auto py-8 px-12 bg-base-100 rounded-md shadow-mdg">
         <h1 class="text-4xl font-bold mb-6">Profile</h1>
+
+        <p>{{Auth::user()->name}}</p>
+        <p>{{Auth::user()->email}}</p>
     </section>
 @endsection
