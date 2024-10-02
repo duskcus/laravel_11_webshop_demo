07ce6bf24f01dd87ad82ce19162d349aa1c765d2
duskcus <60875989+duskcus@users.noreply.github.com>
2024-10-02 14:02:08 +0200

    Update create.blade.php

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -2,7 +2,55 @@
 
 {{-- @SECTION FOR THE START CONTENT --}}
 @section('content')
-    <section>
-        hello world.
+    <section class="w-[30%] mx-auto py-8 px-12 bg-base-100 rounded-md shadow-md">
+
+        <form action="{{ route('products.create') }}" method="POST">
+
+            @csrf
+            @method('GET')
+
+            <div class="form-control mt-4">
+                <label class="label" for="name">
+                    <span class="label-text">Name</span>
+                </label>
+                <input type="text" name="name" id="name" class="input input-bordered w-full" placeholder="Name" required>
+            </div>
+            <div class="form-control mt-4">
+                <label class="label" for="price">
+                    <span class="label-text">Price</span>
+                </label>
+                <input type="text" name="price" id="price" class="input input-bordered w-full" placeholder="Price" required>
+            </div>
+            <div class="form-control mt-4">
+                <label class="label" for="stock">
+                    <span class="label-text">Stock</span>
+                </label>
+                <input type="text" name="stock" id="stock" class="input input-bordered w-full" placeholder="Stock" required>
+            </div>
+            <div class="form-control mt-4">
+                <label class="label" for="description">
+                    <span class="label-text">Description</span>
+                </label>
+                <input type="text" name="description" id="description" class="input input-bordered w-full" placeholder="Description" required>
+            </div>
+            {{-- TODO --}}
+            <div class="form-control mt-4">
+                <label class="label" for="image">
+                    <span class="label-text">Image</span>
+                </label>
+                <input type="url" name="image" id="image" class="input input-bordered w-full" placeholder="Image">
+            </div>
+            <div class="form-control mt-4">
+                <label class="label" for="category">
+                    <span class="label-text">Category</span>
+                </label>
+                <input type="text" name="category" id="category" class="input input-bordered w-full" placeholder="Category" required>
+            </div>
+
+            <div class="mt-6">
+                <button type="submit" class="btn btn-primary w-full">Change</button>
+            </div>
+
+        </form>
     </section>
 @endsection
