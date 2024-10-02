f3e50c28c8d7b0d80ae4d30a447252d1977892de
duskcus <60875989+duskcus@users.noreply.github.com>
2024-10-02 15:30:51 +0200

    Update show.blade.php

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -4,7 +4,7 @@
 @section('content')
     <section>
         <div class="grid grid-cols-2 gap-6 p-6">
-        <img class="h-[300px]" src="{{ $product->image ?? 'https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp' }}" alt="Product Image">
+        <img class="h-[300px]" src="{{ $product->image_url ?? 'https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp' }}" alt="Product Image">
         <div>
             <p class="text-2xl mb-6 capitalize">{{ $product->name }}</p>
             <p>{{ $product->description }}</p>
