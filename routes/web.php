c1113ab9eeb689e1ffe043d31dbf4df5304efa47
duskcus <60875989+duskcus@users.noreply.github.com>
2024-10-03 08:39:20 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\TaskController;
 
 
 // STANDARD
