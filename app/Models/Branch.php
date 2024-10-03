04e58e65e210ced493eff615fa466199bda5b785
duskcus <60875989+duskcus@users.noreply.github.com>
2024-10-03 08:53:33 +0200

    Update Branch.php

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -7,16 +7,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Branch extends Model
 {
-    protected $searchable = [
-        'columns' => [
-            'name' => 50,
-            'street' => 20,
-            'house_number' => 7,
-            'zip_code' => 10,
-            'city' => 20,
-            'country' => 20,
-            'business_number' => 10,
-            'vat_number' => 10,
-        ]
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'branches';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name' => 50,
+        'street' => 20,
+        'house_number' => 7,
+        'zip_code' => 10,
+        'city' => 20,
+        'country' => 20,
+        'business_number' => 10,
+        'vat_number' => 10,
     ];
 }
