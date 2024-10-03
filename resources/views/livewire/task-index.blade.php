6f6a1909523e7dcb518b7874aa84c9ed646f973d
duskcus <60875989+duskcus@users.noreply.github.com>
2024-10-03 11:12:43 +0200

    Update task-index.blade.php

--- resources/views/livewire/task-index.blade.php
+++ resources/views/livewire/task-index.blade.php
@@ -7,18 +7,37 @@
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js'></script>
 
-    <script>
-        // Calendar is seen as an object and it has existing properties in the docs
-        document.addEventListener('DOMContentLoaded', function () {
-            var calendarEl = document.getElementById('calendar');
-            var calendar = new FullCalendar.Calendar(calendarEl, {
-                initialView: 'timeGridWeek',
-                slotMinTime: '8:00:00',
-                slotMaxTime: '18:00:00',
-                firstDay: 1, // Start the week on Monday
-                events: @json($calendarItems),
+        <script>
+            // Calendar is seen as an object and it has existing properties in the docs
+            document.addEventListener('DOMContentLoaded', function () {
+                var calendarEl = document.getElementById('calendar');
+                var calendar = new FullCalendar.Calendar(calendarEl, {
+                    height: 'auto',
+                    initialView: 'timeGridWeek',
+                    slotMinTime: '8:00:00',
+                    slotMaxTime: '18:00:00',
+                    firstDay: 1, // Start the week on Monday
+                    locale: 'nl', // The language is set to Dutch (nl).
+                    selectable: true,
+                    events: @json($calendarItems),
+                    // FUNCTIONS / HANDLERS / CALLBACKS
+                    // Activates when clicking somehwere
+                    // dateClick: function() {
+                    //     alert('a day has been clicked!');
+                    // },
+                    // Actives when clicking an event aka task
+                    eventClick: function(info) {
+                        // You can access the event's details via 'info' parameter
+                        // alert('Event: ' + info.event.title);
+
+                        // Send the event data to Livewire and trigger the modal
+                        Livewire.emit('openModal', {
+                            title: info.event.title,
+                            start: info.event.startStr
+                        });
+                    }
+                });
+                calendar.render();
             });
-            calendar.render();
-        });
-      </script>
+        </script>
 @endpush
