6e0fdb1723661dda712288648959fe75261b6c4c
duskcus <60875989+duskcus@users.noreply.github.com>
2024-10-04 11:08:57 +0200

    Update TaskIndex.php

--- app/Livewire/TaskIndex.php
+++ app/Livewire/TaskIndex.php
@@ -4,26 +4,32 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Task;
+use Illuminate\Support\Facades\Auth;
 
-class TaskIndex extends Component
+class Calendar extends Component
 {
     public function render()
     {
         $calendarItems = [];
 
-        // $appointments = Appointment::with(['client', 'employee'])->get();
-        $tasks = Task::all();
+        // Get the ID of the logged-in user
+        $userId = Auth::id();
 
+        // Retrieve tasks associated with the logged-in user
+        $tasks = Task::whereHas('users', function ($query) use ($userId) {
+            $query->where('user_id', $userId);
+        })->get();
+
+        // convert database data to usable array for both fullcalender and blade
         foreach ($tasks as $task) {
             $calendarItems[] = [
-                'title' => $task->title . ' ('.$task->title.')',
+                'title' => $task->title . ' ('.$task->location.')',
                 'start' => $task->start,
                 'end' => $task->end,
                 'description' => $task->description,
-                'location' => $task->location,
             ];
         }
 
-        return view('livewire.task-index', ['calendarItems' => $calendarItems]);
+        return view('livewire.calendar', ['calendarItems' => $calendarItems]);
     }
 }
